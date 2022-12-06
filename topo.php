1b8a99ff1c3e710affacde789843e813405591e8
Luan Martins <108654763+devluanm@users.noreply.github.com>
2022-12-06 10:45:11 -0300

    Update topo.php

--- topo.php
+++ topo.php
@@ -1,12 +1,19 @@
+
 <?php
-include("config.inc.php");
+
+    include("config.inc.php");
+
 ?>
 
 <header>
     <div class="topo-container">
+        
         <ul class="nav">
+            
             <li><a href="?pg=principal">Home</a></li>
+            
             <?php
+            
             $busca = "Select * from contpaginas order by id";
 
             $todos = mysqli_query($conn, $busca);
@@ -15,17 +22,30 @@ include("config.inc.php");
 
             ?>
                 <li>
+                    
                     <a href="?pg=<?= $dados['link']; ?>"><?= $dados['nome']; ?></a>
+                    
                 </li>
+            
             <?php
             }
+            
             ?>
+            
             <li><a href="?pg=servicos">Serviços</a></li>
+            
             <li><a href="?pg=agendamento">Agendamento</a></li>
+            
             <li><a href="?pg=faleconosco">Fale conosco</a></li>
+            
         </ul>
+        
         <div class="admin nav">
+            
             <li><a href="./admin/login.php"><i id="user" class="fa-solid fa-user"></i>Admin</a></li>
+            
         </div>
+        
     </div>
+    
 </header>
